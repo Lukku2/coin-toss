85ed255ed46b72e2ae9c875432c8240e2a07b6d7
Lukmanul Hakkeem <113803687+Lukku2@users.noreply.github.com>
2025-11-23 17:15:55 +0530

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "coin-toss-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
+];
+
+// Install Event
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Event (Serve from cache first)
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
